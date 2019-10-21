3c3c1da9090c10786dffbf4311529b78a686d7f2
Hemant Kumar <hemant_4000@yahoo.in>
2019-10-21 18:17:51 +0530

    Update main.tf

--- .ci/terraform/main.tf
+++ .ci/terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 //Run number of test case based on count value: say 3 will run 3 test cases 
 variable "instance_count" {
- default = 10
+ default = 1
 }
 
 variable "hostnames" {
