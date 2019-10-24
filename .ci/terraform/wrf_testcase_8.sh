e5abb71c4637b9063d9d286a75de4be2701dddd0
Dave Gill <gill@ucar.edu>
2019-10-24 14:26:25 -0600

    Remove references to non-existing test scripts
    Test 02: NMM, only serial and mpi
    Test 03: chem, only serial and mpi
    Test 08: moving nest, only mpi
    Test 10: hill2d_x, only serial
    
    Also, for Test 08 and 10, since there is no comparison available,
    the last_only_once.csh script is not called.
    
    modified:   wrf_testcase_10.sh
    modified:   wrf_testcase_2.sh
    modified:   wrf_testcase_3.sh
    modified:   wrf_testcase_8.sh

--- .ci/terraform/wrf_testcase_8.sh
+++ .ci/terraform/wrf_testcase_8.sh
@@ -6,6 +6,5 @@ git clone git@github.com:davegill/wrf-coop.git
 cd wrf-coop/
 sed -e "s^_GIT_URL_^$GIT_URL^" -e "s/_GIT_BRANCH_/$GIT_BRANCH/" Dockerfile-sed > Dockerfile
 csh build.csh /home/ubuntu/wrf-stuff/ /home/ubuntu/wrf-stuff/
-date ; ( ./single.csh ; ./test_008s.csh & ./test_008o.csh & ./test_008m.csh & wait ) >output_008 ; date
-date ; ./last_only_once.csh >> output_008 ; date
+date ; ( ./single.csh ; ./test_008m.csh & wait ) >output_008 ; date
 EOF
