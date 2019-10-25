6be266b6092058295c118c885218cff20e0ad4c6
Hemant Kumar <hemant_4000@yahoo.in>
2019-10-25 17:19:53 +0530

    Update wrf_testcase_8.sh

--- .ci/terraform/wrf_testcase_8.sh
+++ .ci/terraform/wrf_testcase_8.sh
@@ -6,5 +6,6 @@ git clone git@github.com:davegill/wrf-coop.git
 cd wrf-coop/
 sed -e "s^_GIT_URL_^$GIT_URL^" -e "s/_GIT_BRANCH_/$GIT_BRANCH/" Dockerfile-sed > Dockerfile
 csh build.csh /home/ubuntu/wrf-stuff/ /home/ubuntu/wrf-stuff/
-date ; ( ./single.csh ; ./test_008m.csh & wait ) >output_008 ; date
+date ; ( ./single.csh ; ./test_008m.csh & wait ) >output_8 ; date
+date ; ./last_only_once.csh >> output_8 ; date
 EOF
