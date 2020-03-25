711101c44cd42dbefa880c7b51b41abfcd98af2d
Dave Gill <gill@ucar.edu>
2020-03-25 08:50:48 -0600

    infinite loop with 16 tests, back to 10

--- .ci/terraform/main.tf
+++ .ci/terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 //Run number of test case based on count value: "default = 16" means run all sixteen builds
 variable "instance_count" {
- default = 16
+ default = 10
 }
 
 variable "hostnames" {
