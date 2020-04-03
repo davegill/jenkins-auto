35cc31d55e59d9f846ec3e3abcee05f02cd9ed12
Dave Gill <gill@ucar.edu>
2020-04-02 21:10:17 -0600

    Eventually allow 18 tests to run. Stick at 10 for now
    modified:   main.tf

--- .ci/terraform/main.tf
+++ .ci/terraform/main.tf
@@ -3,13 +3,13 @@ provider "aws" {
    profile    = "${var.aws_profile}" #.aws/credentials
    }
 
-//Run number of test case based on count value: "default = 16" means run all sixteen builds
+//Run number of test case based on count value: "default = 18" means run all eighteen builds
 variable "instance_count" {
  default = 10
 }
 
 variable "hostnames" {
-   default = ["wrf_testcase_1.sh", "wrf_testcase_2.sh", "wrf_testcase_3.sh","wrf_testcase_4.sh","wrf_testcase_5.sh","wrf_testcase_6.sh","wrf_testcase_7.sh", "wrf_testcase_8.sh","wrf_testcase_9.sh","wrf_testcase_10.sh","wrf_testcase_11.sh","wrf_testcase_12.sh","wrf_testcase_13.sh","wrf_testcase_14.sh","wrf_testcase_15.sh","wrf_testcase_16.sh"]
+   default = ["wrf_testcase_1.sh", "wrf_testcase_2.sh", "wrf_testcase_3.sh","wrf_testcase_4.sh","wrf_testcase_5.sh","wrf_testcase_6.sh","wrf_testcase_7.sh", "wrf_testcase_8.sh","wrf_testcase_9.sh","wrf_testcase_10.sh","wrf_testcase_11.sh","wrf_testcase_12.sh","wrf_testcase_13.sh","wrf_testcase_14.sh","wrf_testcase_15.sh","wrf_testcase_16.sh","wrf_testcase_17.sh","wrf_testcase_18.sh"]
 }
 
 data "template_file" "user-data" {
