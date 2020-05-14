654f3c2e225e5252660e1ccd63534b39a3055bd6
Dave Gill <gill@ucar.edu>
2020-05-14 09:43:46 -0600

    test 19 is for KPP
    modified:   make_NEW_sh.csh
    new file:   wrf_testcase_19.sh

--- .ci/terraform/make_NEW_sh.csh
+++ .ci/terraform/make_NEW_sh.csh
@@ -2,8 +2,8 @@
 
 #	This script makes the wrf_testcase_xx.sh files.
 
-set TEST_NUM = (  1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  )
-set BUILDS   = ( som  m  sm  som som som som  m  som  s  som som som som som som som sm  )
+set TEST_NUM = (  1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19 )
+set BUILDS   = ( som  m  sm  som som som som  m  som  s  som som som som som som som sm  sm )
 
 
 set COUNT = 1
---
+++ .ci/terraform/wrf_testcase_19.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+su - ubuntu << 'EOF'
+wget https://wrf-testcase.s3.amazonaws.com/my_script.sh
+mkdir /home/ubuntu/wrf-stuff
+cd wrf-stuff/
+git clone git@github.com:davegill/wrf-coop.git
+cd wrf-coop/
+sed -e "s^_GIT_URL_^$GIT_URL^" -e "s/_GIT_BRANCH_/$GIT_BRANCH/" Dockerfile-sed > Dockerfile
+sed -e "s^_GIT_URL_^$GIT_URL^" -e "s/_GIT_BRANCH_/$GIT_BRANCH/" Dockerfile-sed-NMM > Dockerfile-NMM
+csh build.csh /home/ubuntu/wrf-stuff/wrf-coop /home/ubuntu/wrf-stuff/wrf-coop
+echo "==============================================================" >  SERIAL
+echo "==============================================================" >> SERIAL
+echo "                         SERIAL START" >> SERIAL
+echo "==============================================================" >> SERIAL
+
+echo "==============================================================" >  OPENMP
+echo "==============================================================" >> OPENMP
+echo "                         OPENMP START" >> OPENMP
+echo "==============================================================" >> OPENMP
+
+echo "==============================================================" >  MPI
+echo "==============================================================" >> MPI
+echo "                         MPI START" >> MPI
+echo "==============================================================" >> MPI
+
+date ; ./single.csh Dockerfile     > output_19 ; date 
+./test_019s.csh > outs & 
+./test_019m.csh > outm & 
+wait 
+cat SERIAL outs MPI outm >> output_19
+date ; ./last_only_once.csh >> output_19 ; date
+rm outs outm 
+rm SERIAL OPENMP MPI 
+EOF
