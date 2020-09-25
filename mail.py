8e56600d8b9fca31d69f6c4127e104ad10b2c3a7
Vikram Jakhar <vikram.jakhr@gmail.com>
2020-09-25 14:41:46 +0530

    dave's email id udpated

--- mail.py
+++ mail.py
@@ -33,7 +33,7 @@ O=sys.argv[21]
 P=sys.argv[22]
 
 AWS_REGION = "us-east-1"
-DAVID_GILL="dave@ucar.edu"
+DAVID_GILL="gill@ucar.edu"
 VIKRAM="vjakhar@scalacomputing.com"
 SCALA="info@scalacomputing.com"
 f = open(FILE,"r")
