cb2d4f345c1179773b9eaf2f68af9b09c2b72dac
Vikram Jakhar <vikram.jakhr@gmail.com>
2020-12-07 18:57:10 +0530

    revert ami id

--- .ci/terraform/vars.tf
+++ .ci/terraform/vars.tf
@@ -22,7 +22,7 @@ variable "devicename" {
 variable "instance_count" {default = 19 }
 variable "instance_name"               { default = "wrf-test" }
 variable "instance_profile"            { default = "WRFS3Role" }
-variable "ami"                         { default = "ami-03eddd68dc3d8a48a" }
+variable "ami"                         { default = "ami-0133bb439aeabb280" }
 variable "availability_zone"           { default = ""   }
 variable "ebs_optimized"               { default = false  }
 variable "instance_type"               { default = "c5.4xlarge" }
