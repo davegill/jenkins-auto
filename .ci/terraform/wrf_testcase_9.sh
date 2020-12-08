f85b726b3597298dd1f249c84a94877fdf3023ec
Vikram Jakhar <vikram.jakhr@gmail.com>
2020-12-08 19:24:41 +0530

    added scala wrf-coop

--- .ci/terraform/wrf_testcase_9.sh
+++ .ci/terraform/wrf_testcase_9.sh
@@ -3,7 +3,7 @@ su - ubuntu << 'EOF'
 wget https://wrf-testcase-staging.s3.amazonaws.com/my_script.sh
 mkdir /home/ubuntu/wrf-stuff
 cd wrf-stuff/
-git clone --branch leave_docker_images https://github.com/davegill/wrf-coop.git
+git clone --branch leave_docker_images https://github.com/scala-computing/wrf-coop.git
 cd wrf-coop/
 sed -e "s^_GIT_URL_^$GIT_URL^" -e "s^_GIT_BRANCH_^$GIT_BRANCH^" Dockerfile-sed > Dockerfile
 sed -e "s^_GIT_URL_^$GIT_URL^" -e "s^_GIT_BRANCH_^$GIT_BRANCH^" Dockerfile-sed-NMM > Dockerfile-NMM
