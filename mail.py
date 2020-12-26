695241031402f6302080d09e4ba14fb646f0e510
vlakshmanan-scala <vlakshmanan@scalacomputing.com>
2020-12-25 22:57:39 -0700

    email reverted

--- mail.py
+++ mail.py
@@ -34,7 +34,7 @@ P=sys.argv[22]
 
 AWS_REGION = "us-east-1"
 DAVID_GILL="hstone@scalacomputing.com"
-VIKRAM="76571148+varunL93@users.noreply.github.com"
+VIKRAM="vlakshmanan@scalacomputing.com"
 SCALA="ncar-dev@scalacomputing.com"
 f = open(FILE,"r")
 contents=f.read()
