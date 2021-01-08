fe970eb258e6d7e9a78855f92d44dc41f2176bd4
vlakshmanan-scala <vlakshmanan@scalacomputing.com>
2021-01-08 11:39:55 -0500

    Just checking

--- mail.py
+++ mail.py
@@ -34,7 +34,8 @@ P=sys.argv[22]
 
 AWS_REGION = "us-east-1"
 DAVID_GILL="hstone@scalacomputing.com"
-VIKRAM="vlakshmanan@scalacomputing.com"
+VIKRAM="varunlakshma@usf.edu"
+#"vlakshmanan@scalacomputing.com"
 SCALA="ncar-dev@scalacomputing.com"
 f = open(FILE,"r")
 contents=f.read()
