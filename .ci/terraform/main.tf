b5f1c85c1f2e634c3698dd1cd0a084afb188860c
vlakshmanan-scala <vlakshmanan@scalacomputing.com>
2021-01-23 14:25:16 -0500

    changes instance type

--- .ci/terraform/main.tf
+++ .ci/terraform/main.tf
@@ -2,7 +2,8 @@ provider "aws" {
    region     = "${var.region}"
    profile    = "${var.aws_profile}" #.aws/credentials
    }
-   
+
+/* //original
 variable "hostnames" {
    default = ["wrf_testcase_1.sh", "wrf_testcase_2.sh", "wrf_testcase_3.sh","wrf_testcase_4.sh","wrf_testcase_5.sh","wrf_testcase_6.sh","wrf_testcase_7.sh", "wrf_testcase_8.sh","wrf_testcase_9.sh","wrf_testcase_10.sh","wrf_testcase_11.sh","wrf_testcase_12.sh","wrf_testcase_13.sh","wrf_testcase_14.sh","wrf_testcase_15.sh","wrf_testcase_16.sh","wrf_testcase_17.sh","wrf_testcase_18.sh","wrf_testcase_19.sh"]
 }
@@ -14,6 +15,67 @@ data "template_file" "user-data" {
 
 resource "aws_instance" "application" {
   count                         = "${var.instance_count}"
+  ami                           = "${var.ami}"
+  iam_instance_profile          = "${var.instance_profile}"
+  ebs_block_device              = {
+     device_name = "${var.devicename}" 
+     volume_size = "${var.volumesize}"
+                                  }
+  availability_zone             = "${var.availability_zone}"
+  ebs_optimized                 = "${var.ebs_optimized}"
+  instance_type                 = "${var.instance_type}"
+  key_name                      = "${var.key_name}"
+  monitoring                    = "${var.monitoring}"
+  vpc_security_group_ids        = ["${var.security_group_ids}"]
+  subnet_id                     = "${var.subnet_id}"
+  associate_public_ip_address   = "${var.associate_public_ip_address}"
+  user_data                     = "${element(data.template_file.user-data.*.rendered, count.index)}"
+  tags                          = "${merge(var.tags, map("Name", format("%s", var.instance_name)))}"
+
+}
+*/
+
+variable "hostnames_1" {
+   default = ["wrf_testcase_1.sh","wrf_testcase_11.sh","wrf_testcase_12.sh","wrf_testcase_13.sh","wrf_testcase_14.sh","wrf_testcase_15.sh","wrf_testcase_16.sh","wrf_testcase_17.sh","wrf_testcase_18.sh","wrf_testcase_19.sh"]
+}
+
+data "template_file" "user-data" {
+     count = "${length(var.hostnames_1)}"
+     template = "${file("${element(var.hostnames_1, count.index)}")}"
+}
+
+resource "aws_instance" "application" {
+  //count                         = "${var.instance_count}"
+  ami                           = "${var.ami}"
+  iam_instance_profile          = "${var.instance_profile}"
+  ebs_block_device              = {
+     device_name = "${var.devicename}" 
+     volume_size = "${var.volumesize}"
+                                  }
+  availability_zone             = "${var.availability_zone}"
+  ebs_optimized                 = "${var.ebs_optimized}"
+  instance_type                 = "${var.instance_type_1}"
+  key_name                      = "${var.key_name}"
+  monitoring                    = "${var.monitoring}"
+  vpc_security_group_ids        = ["${var.security_group_ids}"]
+  subnet_id                     = "${var.subnet_id}"
+  associate_public_ip_address   = "${var.associate_public_ip_address}"
+  user_data                     = "${element(data.template_file.user-data.*.rendered, count.index)}"
+  tags                          = "${merge(var.tags, map("Name", format("%s", var.instance_name)))}"
+
+}
+
+variable "hostnames" {
+   default = [ "wrf_testcase_2.sh", "wrf_testcase_3.sh","wrf_testcase_4.sh","wrf_testcase_5.sh","wrf_testcase_6.sh","wrf_testcase_7.sh", "wrf_testcase_8.sh","wrf_testcase_9.sh","wrf_testcase_10.sh"]
+}
+
+data "template_file" "user-data" {
+     count = "${length(var.hostnames)}"
+     template = "${file("${element(var.hostnames, count.index)}")}"
+}
+
+resource "aws_instance" "application" {
+  //count                         = "${var.instance_count}"
   ami                           = "${var.ami}"
   iam_instance_profile          = "${var.instance_profile}"
   ebs_block_device              = {
