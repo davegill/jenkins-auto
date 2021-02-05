f2db9aa687ddfa61972558115773429dbe79cf93
vlakshmanan-scala <vlakshmanan@scalacomputing.com>
2021-02-05 08:43:58 -0500

    serial test

--- .ci/terraform/wrf_testcase_1.sh
+++ .ci/terraform/wrf_testcase_1.sh
@@ -25,8 +25,7 @@ echo "==============================================================" >> MPI
 
 date ; ./single_init.csh Dockerfile     wrf_regtest    > output_1 ; date 
 ./test_001s.csh > outs & 
-./test_001o.csh > outo & 
-./test_001m.csh > outm & 
+
 wait 
 ./single_end.csh wrf_regtest    >> output_1 ; date 
 cat SERIAL outs OPENMP outo MPI outm >> output_1
