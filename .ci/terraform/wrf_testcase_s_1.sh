3c6d45cad4f37b8a18dceb23291dde0e0cb04663
vlakshmanan-scala <vlakshmanan@scalacomputing.com>
2021-02-08 14:32:06 -0500

    back to s

--- .ci/terraform/wrf_testcase_s_1.sh
+++ .ci/terraform/wrf_testcase_s_1.sh
@@ -6,6 +6,10 @@ cd wrf-stuff/
 git clone --branch leave_docker_images https://github.com/scala-computing/wrf-coop.git
 cd wrf-coop/
 sed -e "s^_GIT_URL_^$GIT_URL^" -e "s^_GIT_BRANCH_^$GIT_BRANCH^" Dockerfile-sed > Dockerfile
+
+cat Dockerfile
+ls -ls
+echo $GIT_URL
 sed -e "s^_GIT_URL_^$GIT_URL^" -e "s^_GIT_BRANCH_^$GIT_BRANCH^" Dockerfile-sed-NMM > Dockerfile-NMM
 csh build.csh /home/ubuntu/wrf-stuff/wrf-coop /home/ubuntu/wrf-stuff/wrf-coop
 echo "==============================================================" >  SERIAL
