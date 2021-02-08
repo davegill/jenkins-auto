4be374b056b89658b915104e14244ad5305c10d1
vlakshmanan-scala <vlakshmanan@scalacomputing.com>
2021-02-08 17:08:18 -0500

    back to _s

--- .ci/terraform/wrf_testcase_s_1.sh
+++ .ci/terraform/wrf_testcase_s_1.sh
@@ -6,10 +6,6 @@ cd wrf-stuff/
 git clone --branch leave_docker_images https://github.com/scala-computing/wrf-coop.git
 cd wrf-coop/
 sed -e "s^_GIT_URL_^$GIT_URL^" -e "s^_GIT_BRANCH_^$GIT_BRANCH^" Dockerfile-sed > Dockerfile
-
-cat Dockerfile
-ls -ls
-echo $GIT_URL
 sed -e "s^_GIT_URL_^$GIT_URL^" -e "s^_GIT_BRANCH_^$GIT_BRANCH^" Dockerfile-sed-NMM > Dockerfile-NMM
 csh build.csh /home/ubuntu/wrf-stuff/wrf-coop /home/ubuntu/wrf-stuff/wrf-coop
 echo "==============================================================" >  SERIAL
@@ -17,12 +13,12 @@ echo "==============================================================" >> SERIAL
 echo "                         SERIAL START" >> SERIAL
 echo "==============================================================" >> SERIAL
 
-date ; ./single_init.csh Dockerfile     wrf_regtest    > outputs_1 ; date 
+date ; ./single_init.csh Dockerfile     wrf_regtest    > output_s_1 ; date 
 ./test_001s.csh > outs &
 
 wait 
-./single_end.csh wrf_regtest    >> outputs_1 ; date 
-cat SERIAL outs  >> outputs_1
+./single_end.csh wrf_regtest    >> output_s_1 ; date 
+cat SERIAL outs  >> output_s_1
 
 rm outs 
 rm SERIAL OPENMP MPI 
