7f053b7dbc9988784b72b39141423961a872dbb6
vlakshmanan-scala <vlakshmanan@scalacomputing.com>
2021-02-09 12:49:57 -0500

    testcase j

---
+++ .ci/terraform/wrf_testcase_25.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+su - ubuntu << 'EOF'
+wget https://wrf-testcase-staging.s3.amazonaws.com/my_script.sh
+mkdir /home/ubuntu/wrf-stuff
+cd wrf-stuff/
+git clone --branch leave_docker_images https://github.com/scala-computing/wrf-coop.git
+cd wrf-coop/
+sed -e "s^_GIT_URL_^$GIT_URL^" -e "s^_GIT_BRANCH_^$GIT_BRANCH^" Dockerfile-sed > Dockerfile
+sed -e "s^_GIT_URL_^$GIT_URL^" -e "s^_GIT_BRANCH_^$GIT_BRANCH^" Dockerfile-sed-NMM > Dockerfile-NMM
+csh build.csh /home/ubuntu/wrf-stuff/wrf-coop /home/ubuntu/wrf-stuff/wrf-coop
+
+echo "==============================================================" >  MPI
+echo "==============================================================" >> MPI
+echo "                         MPI START" >> MPI
+echo "==============================================================" >> MPI
+
+date ; ./single_init.csh Dockerfile     wrf_regtest    > output_25 ; date  
+./test_003m.csh > outm &
+wait 
+./single_end.csh wrf_regtest    >> output_25 ; date 
+cat MPI outm >> output_25
+
+rm outm 
+rm SERIAL OPENMP MPI 
+EOF
---
+++ .ci/terraform/wrf_testcase_52.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+su - ubuntu << 'EOF'
+wget https://wrf-testcase-staging.s3.amazonaws.com/my_script.sh
+mkdir /home/ubuntu/wrf-stuff
+cd wrf-stuff/
+git clone --branch leave_docker_images https://github.com/scala-computing/wrf-coop.git
+cd wrf-coop/
+sed -e "s^_GIT_URL_^$GIT_URL^" -e "s^_GIT_BRANCH_^$GIT_BRANCH^" Dockerfile-sed > Dockerfile
+sed -e "s^_GIT_URL_^$GIT_URL^" -e "s^_GIT_BRANCH_^$GIT_BRANCH^" Dockerfile-sed-NMM > Dockerfile-NMM
+csh build.csh /home/ubuntu/wrf-stuff/wrf-coop /home/ubuntu/wrf-stuff/wrf-coop
+echo "==============================================================" >  SERIAL
+echo "==============================================================" >> SERIAL
+echo "                         SERIAL START" >> SERIAL
+echo "==============================================================" >> SERIAL
+
+date ; ./single_init.csh Dockerfile     wrf_regtest    > output_52 ; date 
+./test_014s.csh > outs & 
+
+wait 
+./single_end.csh wrf_regtest    >> output_52 ; date 
+cat SERIAL outs >> output_52
+
+rm outs 
+rm SERIAL OPENMP MPI 
+EOF
