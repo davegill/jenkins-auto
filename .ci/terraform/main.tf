0ea9cfc01339878bf0b6425f2c54cb4412bf5223
vlakshmanan-scala <vlakshmanan@scalacomputing.com>
2021-02-09 11:37:13 -0500

    som

--- .ci/terraform/main.tf
+++ .ci/terraform/main.tf
@@ -6,8 +6,11 @@ provider "aws" {
 # variable "hostnames" {
 #    default = ["wrf_testcase_1.sh", "wrf_testcase_2.sh", "wrf_testcase_3.sh","wrf_testcase_4.sh","wrf_testcase_5.sh","wrf_testcase_6.sh","wrf_testcase_7.sh", "wrf_testcase_8.sh","wrf_testcase_9.sh","wrf_testcase_10.sh","wrf_testcase_11.sh","wrf_testcase_12.sh","wrf_testcase_13.sh","wrf_testcase_14.sh","wrf_testcase_15.sh","wrf_testcase_16.sh","wrf_testcase_17.sh","wrf_testcase_18.sh","wrf_testcase_19.sh"]
 # }
+# variable "hostnames" {
+#    default = ["wrf_testcase_s_1.sh", "wrf_testcase_s_3.sh","wrf_testcase_s_4.sh","wrf_testcase_s_5.sh","wrf_testcase_s_6.sh","wrf_testcase_s_7.sh", "wrf_testcase_s_9.sh","wrf_testcase_s_10.sh","wrf_testcase_s_11.sh","wrf_testcase_s_12.sh","wrf_testcase_s_13.sh","wrf_testcase_s_14.sh","wrf_testcase_s_15.sh","wrf_testcase_s_16.sh","wrf_testcase_s_17.sh","wrf_testcase_s_18.sh","wrf_testcase_s_19.sh"]
+# }
 variable "hostnames" {
-   default = ["wrf_testcase_s_1.sh", "wrf_testcase_s_3.sh","wrf_testcase_s_4.sh","wrf_testcase_s_5.sh","wrf_testcase_s_6.sh","wrf_testcase_s_7.sh", "wrf_testcase_s_9.sh","wrf_testcase_s_10.sh","wrf_testcase_s_11.sh","wrf_testcase_s_12.sh","wrf_testcase_s_13.sh","wrf_testcase_s_14.sh","wrf_testcase_s_15.sh","wrf_testcase_s_16.sh","wrf_testcase_s_17.sh","wrf_testcase_s_18.sh","wrf_testcase_s_19.sh"]
+   default = ["wrf_testcase_s_1.sh", "wrf_testcase_o_1.sh", "wrf_testcase_m_1.sh", "wrf_testcase_m_2.sh", "wrf_testcase_s_3.sh", "wrf_testcase_m_3.sh","wrf_testcase_s_4.sh","wrf_testcase_o_4.sh","wrf_testcase_m_4.sh","wrf_testcase_s_5.sh","wrf_testcase_o_5.sh","wrf_testcase_m_5.sh","wrf_testcase_s_6.sh","wrf_testcase_o_6.sh","wrf_testcase_m_6.sh","wrf_testcase_s_7.sh","wrf_testcase_o_7.sh","wrf_testcase_m_7.sh","wrf_testcase_m_8.sh", "wrf_testcase_s_9.sh","wrf_testcase_o_9.sh","wrf_testcase_m_9.sh","wrf_testcase_s_10.sh","wrf_testcase_s_11.sh","wrf_testcase_o_11.sh","wrf_testcase_m_11.sh","wrf_testcase_s_12.sh","wrf_testcase_o_12.sh","wrf_testcase_m_12.sh","wrf_testcase_s_13.sh","wrf_testcase_o_13.sh","wrf_testcase_m_13.sh","wrf_testcase_s_14.sh","wrf_testcase_o_14.sh","wrf_testcase_m_14.sh","wrf_testcase_s_15.sh","wrf_testcase_o_15.sh","wrf_testcase_m_15.sh","wrf_testcase_s_16.sh","wrf_testcase_o_16.sh","wrf_testcase_m_16.sh","wrf_testcase_s_17.sh","wrf_testcase_o_17.sh","wrf_testcase_m_17.sh","wrf_testcase_s_18.sh","wrf_testcase_m_18.sh","wrf_testcase_s_19.sh","wrf_testcase_m_19.sh"]
 }
 
 data "template_file" "user-data" {
