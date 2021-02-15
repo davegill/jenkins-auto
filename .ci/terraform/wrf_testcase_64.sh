92cf0b4d5108238548283cc115f2f2d3038cb3ae
vlakshmanan-scala <vlakshmanan@scalacomputing.com>
2021-02-15 08:32:02 -0500

    upload_script

--- .ci/terraform/wrf_testcase_64.sh
+++ .ci/terraform/wrf_testcase_64.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 su - ubuntu << 'EOF'
-wget https://wrf-testcase-staging.s3.amazonaws.com/my_script.sh
+wget https://wrf-testcase-staging.s3.amazonaws.com/upload_script.sh
 mkdir /home/ubuntu/wrf-stuff
 cd wrf-stuff/
 git clone --branch leave_docker_images https://github.com/scala-computing/wrf-coop.git
@@ -8,19 +8,18 @@ cd wrf-coop/
 sed -e "s^_GIT_URL_^$GIT_URL^" -e "s^_GIT_BRANCH_^$GIT_BRANCH^" Dockerfile-sed > Dockerfile
 sed -e "s^_GIT_URL_^$GIT_URL^" -e "s^_GIT_BRANCH_^$GIT_BRANCH^" Dockerfile-sed-NMM > Dockerfile-NMM
 csh build.csh /home/ubuntu/wrf-stuff/wrf-coop /home/ubuntu/wrf-stuff/wrf-coop
-
-echo "==============================================================" >  MPI
-echo "==============================================================" >> MPI
-echo "                         MPI START" >> MPI
-echo "==============================================================" >> MPI
+echo "==============================================================" >  SERIAL
+echo "==============================================================" >> SERIAL
+echo "                         SERIAL START" >> SERIAL
+echo "==============================================================" >> SERIAL
 
 date ; ./single_init.csh Dockerfile     wrf_regtest    > output_64 ; date 
+./test_018s.csh > outs & 
 
-./test_018m.csh > outm & 
 wait 
 ./single_end.csh wrf_regtest    >> output_64 ; date 
-cat MPI outm >> output_64
+cat SERIAL outs >> output_64
 
-rm outm 
+rm outs 
 rm SERIAL OPENMP MPI 
 EOF
