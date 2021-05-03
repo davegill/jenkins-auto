b1a161a74beab2d5051f633c9140008ca4f00246
vlakshmanan-scala <vlakshmanan@scalacomputing.com>
2021-05-03 11:48:00 -0400

    vpc_security_group_ids syntax fix

--- .ci/terraform/main.tf
+++ .ci/terraform/main.tf
@@ -35,7 +35,7 @@ resource "aws_instance" "application" {
   key_name                      = "${var.key_name}"
   monitoring                    = "${var.monitoring}"
   vpc_security_group_ids        = [
-     var.security_group_ids
+     var.security_group_ids,
    ]
   subnet_id                     = "${var.subnet_id}"
   associate_public_ip_address   = "${var.associate_public_ip_address}"
