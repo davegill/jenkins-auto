712b998d957792a1b8393424695c6aa38ef7db44
vlakshmanan-scala <vlakshmanan@scalacomputing.com>
2021-11-08 15:57:04 -0600

    Commented associated public ip

--- .ci/terraform/main.tf
+++ .ci/terraform/main.tf
@@ -39,7 +39,7 @@ instance_type                 = "${count.index <= 1  ? var.instance_type_1 : var
   monitoring                    = "${var.monitoring}"
   vpc_security_group_ids        = var.security_group_ids
   subnet_id                     = "${var.subnet_id}"
-  associate_public_ip_address   = "${var.associate_public_ip_address}"
+#   associate_public_ip_address   = "${var.associate_public_ip_address}"
   user_data                     = "${element(data.template_file.user-data.*.rendered, count.index)}"
   tags                          = "${merge(var.tags, map("Name", format("%s", var.instance_name)))}"
 
