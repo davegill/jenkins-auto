6e6109e6223c82ccdd910a6cf032adbbf70474ed
vlakshmanan <vlakshmanan@scalacomputing.com>
2021-12-13 13:00:34 -0700

    running all on 4xlarge

--- .ci/terraform/vars.tf
+++ .ci/terraform/vars.tf
@@ -18,18 +18,19 @@ variable "devicename" {
 
 
 # EC2 variables
-//Run number of test case based on count value: "default = 18" means run all eighteen builds
-variable "instance_count" {default = 45 }
+//Run number of test case based on count value: "default = 58" means run all builds except KPP
+#DAVE - If this is set to "1", then only wrf_testcase_1.sh is run
+variable "instance_count" {default =58 }
 variable "instance_name"               { default = "wrf-test" }
 variable "instance_profile"            { default = "WRFS3Role" }
 
-variable "ami"                         { default = "ami-0cd4a8daf73c05400" }  #fifteenthtry
+variable "ami"                         { default = "ami-0cd4a8daf73c05400" }  #fifteenthtry & fourteenthtry
 # variable "ami"                         { default = "ami-023f6d664346f3e84" }  #fourteenthtry
 # variable "ami"                         { default = "ami-06e599aaf4015dafd" }  #thirteenthtry & sixthtry
 variable "availability_zone"           { default = ""   }
 variable "ebs_optimized"               { default = false  }
-variable "instance_type_1"             { default = "c5a.4xlarge" }
-variable "instance_type"               { default = "c5a.xlarge" }
+variable "instance_type_1"             { default = "c5a.4xlarge" }   # DAVE - larger for chem builds
+variable "instance_type"               { default = "c5a.xlarge" }    # DAVE - smaller machines
 # variable "instance_type"               { default = "t3.xlarge" }
 variable "key_name"                    { default = "jenkins" }
 variable "monitoring"                  { default = false  }
