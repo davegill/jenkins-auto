fc2fa80b26c24386ede33fdc4ea82f09701cd7e1
vlakshmanan <vlakshmanan@scalacomputing.com>
2024-03-13 12:46:40 -0600

    copy scripts - update

--- scripts/copy-from-s3.sh
+++ scripts/copy-from-s3.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 BUILD=$1
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-sudo rm -rf /home/centos/wrf-test-output/*
+sudo rm -rf ${HOME}/wrf-test-output/*
 
-sudo mkdir -p /home/centos/wrf-test-output/${BUILD}
+sudo mkdir -p ${HOME}/wrf-test-output/${BUILD}
 
-sudo -S aws s3 cp s3://wrf-testcase/raw_output/${BUILD}/ /home/centos/wrf-test-output/${BUILD} --region us-east-1 --recursive
+if [[ ${BRANCH} == "master" ]];
+then
+    bucket-name="wrf-testcase"
+    sudo -S aws s3 cp s3://${bucket-name}/raw_output/${BUILD}/ ${HOME}/wrf-test-output/${BUILD} --region us-east-1 --recursive
+elif [[ ${BRANCH} == "staging" ]];
+then
+    bucket-name=wrf-testcase-staging
+    sudo -S aws s3 cp s3://${bucket-name}/raw_output/${BUILD}/ ${HOME}/wrf-test-output/${BUILD} --region us-east-1 --recursive
+fi
